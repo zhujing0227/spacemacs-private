2dd3ecd7fd0d82f39b7dcd38545ad66e39de04f5
zhujing12 <zhujing12@maoyan.com>
2020-02-18 14:45:43 +0800

    window number

--- init.el
+++ init.el
@@ -59,7 +59,6 @@ This function should only modify configuration layer settings."
                         '(spacemacs/window-split-double-columns
                           spacemacs/window-split-triple-columns
                           spacemacs/window-split-grid))
-     spacemacs-modeline
      (git :variables
           git-magit-status-fullscreen t
           magit-push-always-verify nil
@@ -125,12 +124,12 @@ This function should only modify configuration layer settings."
    ;; To use a local version of a package, use the `:location' property:
    ;; '(your-package :location "~/path/to/your-package/")
    ;; Also include the dependencies as they will not be resolved automatically.
-   dotspacemacs-additional-packages '(eyebrowse keyfreq org-preview-html sicp ssh-agency anki-editor)
+   dotspacemacs-additional-packages '(window-numbering eyebrowse keyfreq org-preview-html sicp ssh-agency anki-editor)
 
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    dotspacemacs-excluded-packages
-   '(ensime org-projectile org-brain magit-gh-pulls magit-gitflow  evil-mc realgud tern company-tern
+   '(ensime org-projectile spaceline org-brain magit-gh-pulls magit-gitflow  evil-mc realgud tern company-tern
                     evil-args evil-ediff evil-exchange evil-unimpaired
                     evil-indent-plus volatile-highlights 
                     spaceline holy-mode skewer-mode rainbow-delimiters
@@ -510,7 +509,6 @@ dump."
   )
 
 (defun dotspacemacs/user-init ()
-  (turn-on-fci-mode)
   
   (setq-default
    configuration-layer-elpa-archives
@@ -542,6 +540,9 @@ dump."
   )
 
 (defun dotspacemacs/user-config ()
+  (require 'window-numbering)
+  (window-numbering-mode 1)
+
   ;; 统计常用命令
   (require 'keyfreq)
   (keyfreq-mode 1)
@@ -690,8 +691,161 @@ dump."
 
            (add-hook 'projectile-mode-hook '(lambda () (remove-hook 'find-file-hook #'projectile-find-file-hook-function)))))
 
-  (setq exec-path (cons "/Users/lionqu/.nvm/versions/node/v10.16.0/bin/" exec-path))
-  (setenv "PATH" (concat "/Users/lionqu/.nvm/versions/node/v10.16.0/bin:" (getenv "PATH")))
+  (defun zilongshanren/update-persp-name ()
+    (when (bound-and-true-p persp-mode)
+      ;; There are multiple implementations of
+      ;; persp-mode with different APIs
+      (progn
+        (or (not (string= persp-nil-name (safe-persp-name (get-frame-persp))))
+            "Default")
+        (let ((name (safe-persp-name (get-frame-persp))))
+          (propertize (concat "[" name "] ")
+                      'face 'font-lock-preprocessor-face
+                      'help-echo "Current Layout name.")))))
+
+
+  (defun spaceline--unicode-number (str)
+    "Return a nice unicode representation of a single-digit number STR."
+    (cond
+     ((string= "1" str) "➊")
+     ((string= "2" str) "➋")
+     ((string= "3" str) "➌")
+     ((string= "4" str) "➍")
+     ((string= "5" str) "➎")
+     ((string= "6" str) "➏")
+     ((string= "7" str) "➐")
+     ((string= "8" str) "➑")
+     ((string= "9" str) "➒")
+     ((string= "0" str) "➓")))
+
+  (defun window-number-mode-line ()
+    "The current window number. Requires `window-numbering-mode' to be enabled."
+    (when (bound-and-true-p window-numbering-mode)
+      (let* ((num (window-numbering-get-number))
+             (str (when num (int-to-string num))))
+        (spaceline--unicode-number str))))
+
+  (defun mode-line-fill (face reserve)
+    "Return empty space using FACE and leaving RESERVE space on the right."
+    (unless reserve
+      (setq reserve 20))
+    (when (and window-system (eq 'right (get-scroll-bar-mode)))
+      (setq reserve (- reserve 3)))
+    (propertize " "
+                'display `((space :align-to
+                                  (- (+ right right-fringe right-margin) ,reserve)))
+                'face face))
+
+  (defun buffer-encoding-abbrev ()
+    "The line ending convention used in the buffer."
+    (let ((buf-coding (format "%s" buffer-file-coding-system)))
+      (if (string-match "\\(dos\\|unix\\|mac\\)" buf-coding)
+          (match-string 1 buf-coding)
+        buf-coding)))
+
+  (setq my-flycheck-mode-line
+        '(:eval
+          (pcase flycheck-last-status-change
+            (`not-checked nil)
+            (`no-checker (propertize " -" 'face 'warning))
+            (`running (propertize " ✷" 'face 'success))
+            (`errored (propertize " !" 'face 'error))
+            (`finished
+             (let* ((error-counts (flycheck-count-errors flycheck-current-errors))
+                    (no-errors (cdr (assq 'error error-counts)))
+                    (no-warnings (cdr (assq 'warning error-counts)))
+                    (face (cond (no-errors 'error)
+                                (no-warnings 'warning)
+                                (t 'success))))
+               (propertize (format "[%s/%s]" (or no-errors 0) (or no-warnings 0))
+                           'face face)))
+            (`interrupted " -")
+            (`suspicious '(propertize " ?" 'face 'warning)))))
+
+  (setq-default mode-line-format
+                (list
+                 " %1"
+                 '(:eval (propertize
+                          (window-number-mode-line)
+                          'face
+                          'font-lock-type-face))
+                 " "
+                 '(:eval (zilongshanren/update-persp-name))
+
+                 "%1 "
+                 ;; the buffer name; the file name as a tool tip
+                 '(:eval (propertize "%b " 'face 'font-lock-keyword-face
+                                     'help-echo (buffer-file-name)))
+
+
+                 " [" ;; insert vs overwrite mode, input-method in a tooltip
+                 '(:eval (propertize (if overwrite-mode "Ovr" "Ins")
+                                     'face 'font-lock-preprocessor-face
+                                     'help-echo (concat "Buffer is in "
+                                                        (if overwrite-mode
+                                                            "overwrite"
+                                                          "insert") " mode")))
+
+                 ;; was this buffer modified since the last save?
+                 '(:eval (when (buffer-modified-p)
+                           (concat ","  (propertize "Mod"
+                                                    'face 'font-lock-warning-face
+                                                    'help-echo "Buffer has been modified"))))
+
+                 ;; is this buffer read-only?
+                 '(:eval (when buffer-read-only
+                           (concat ","  (propertize "RO"
+                                                    'face 'font-lock-type-face
+                                                    'help-echo "Buffer is read-only"))))
+                 "] "
+
+                 ;; anzu
+                 anzu--mode-line-format
+
+                 ;; relative position, size of file
+                 "["
+                 (propertize "%p" 'face 'font-lock-constant-face) ;; % above top
+                 "/"
+                 (propertize "%I" 'face 'font-lock-constant-face) ;; size
+                 "] "
+
+                 ;; the current major mode for the buffer.
+                 '(:eval (propertize "%m" 'face 'font-lock-string-face
+                                     'help-echo buffer-file-coding-system))
+
+                 "%1 "
+                 my-flycheck-mode-line
+                 "%1 "
+                 ;; evil state
+                 '(:eval evil-mode-line-tag)
+
+                 ;; minor modes
+                 minor-mode-alist
+                 " "
+                 ;; git info
+                 `(vc-mode vc-mode)
+
+                 " "
+
+                 ;; global-mode-string goes in mode-line-misc-info
+                 mode-line-misc-info
+
+                 (mode-line-fill 'mode-line 20)
+
+                 ;; line and column
+                 "(" ;; '%02' to set to 2 chars at least; prevents flickering
+                 (propertize "%02l" 'face 'font-lock-type-face) ","
+                 (propertize "%02c" 'face 'font-lock-type-face)
+                 ") "
+
+                 '(:eval (buffer-encoding-abbrev))
+                 mode-line-end-spaces
+                 ;; add the time, with the date and the emacs uptime in the tooltip
+                 ;; '(:eval (propertize (format-time-string "%H:%M")
+                 ;;                     'help-echo
+                 ;;                     (concat (format-time-string "%c; ")
+                 ;;                             (emacs-uptime "Uptime:%hh"))))
+                 ))
 
   (defun counsel-locate-cmd-es (input)
     "Return a shell command based on INPUT."
